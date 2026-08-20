fa0f405b7bc972a633e6f98bad853c26e28a9770
Sr-tetao <luluoctavio7@gmail.com>
2026-08-20 19:27:39 -0300

    Update apptowill.py

--- apptowill.py
+++ apptowill.py
@@ -1,4 +1,4 @@
-```python
+```text
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
